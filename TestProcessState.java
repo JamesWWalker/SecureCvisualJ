41bbbc0c4be8221e3c608859f873735d080deeaf
JamesWWalker <jwwalker@mtu.edu>
2017-08-01 21:30:11 -0400

    ProcessStateTest now tests applying changes backwards as well

--- TestProcessState.java
+++ TestProcessState.java
@@ -16,6 +16,7 @@ public class TestProcessState {
     // main:x2                       2      asm5   jax=30, jbx=21, jcx=22
     //
     // Go forwards and backwards and make sure the state is correct all the time
+    // NOTE WELL: It will be necessary to build two sets of deltas: one for forward, one for reverse
 
     VariableDelta x1 = new VariableDelta("int", "main", "x", "1", 500, -1);
     VariableDelta y = new VariableDelta("int", "alpha", "y", "2", 600, -1);
@@ -71,6 +72,24 @@ public class TestProcessState {
     delta5stack.add(main);
     ProcessState delta5 = new ProcessState(2, "asm5", delta5regs, delta5vars, delta5stack);
 
+    TreeMap<String, VariableDelta> delta4Rvars = new TreeMap<>();
+    delta4Rvars.put("alpha,y", y);
+    ProcessState delta4R = new ProcessState(6, "asm4", delta4regs, delta4Rvars, delta4stack);
+
+    TreeMap<String, VariableDelta> delta3Rvars = new TreeMap<>();
+    TreeMap<String, String> delta3Rregs = new TreeMap<>();
+    delta3Rregs.put("jax", "10");
+    delta3Rvars.put("beta,z", z);
+    ProcessState delta3R = new ProcessState(9, "asm3", delta3Rregs, delta3Rvars, delta3stack);
+
+    TreeMap<String, VariableDelta> delta2Rvars = new TreeMap<>();
+    TreeMap<String, String> delta2Rregs = new TreeMap<>();
+    delta2Rregs.put("jcx", "12");
+    ProcessState delta2R = new ProcessState(5, "asm2", delta2Rregs, delta2Rvars, delta2stack);
+
+    TreeMap<String, VariableDelta> delta1Rvars = new TreeMap<>();
+    ProcessState delta1R = new ProcessState(1, "asm1", delta1regs, delta1Rvars, delta1stack);
+
     // Construction complete, now start applying deltas and checking the results
     ProcessState current = new ProcessState();
 
@@ -110,6 +129,33 @@ public class TestProcessState {
     checkVariables.put("main,x", new VariableDelta(x2));
     checkStack.remove(checkStack.size()-1);
     errors += verifyProcessState(current, 2, "asm5", checkRegisters, checkVariables, checkStack);
+
+    // Now go backwards
+    ProcessState.applyDelta(current, delta4R);
+    checkRegisters.put("jax", "20");
+    checkVariables.put("alpha,y", new VariableDelta(y));
+    checkStack.add(new ActivationRecord("alpha"));
+    errors += verifyProcessState(current, 6, "asm4", checkRegisters, checkVariables, checkStack);
+
+    ProcessState.applyDelta(current, delta3R);
+    checkRegisters.put("jax", "10");
+    checkRegisters.put("jcx", "22");
+    checkVariables.put("beta,z", new VariableDelta(z));
+    checkStack.add(new ActivationRecord("beta"));
+    errors += verifyProcessState(current, 9, "asm3", checkRegisters, checkVariables, checkStack);
+
+    ProcessState.applyDelta(current, delta2R);
+    checkRegisters.put("jbx", "21");
+    checkRegisters.put("jcx", "12");
+    checkVariables.remove("beta,z");
+    checkStack.remove(checkStack.size()-1);
+    errors += verifyProcessState(current, 5, "asm2", checkRegisters, checkVariables, checkStack);
+
+    ProcessState.applyDelta(current, delta1R);
+    checkRegisters.put("jbx", "11");
+    checkVariables.remove("alpha,y");
+    checkStack.remove(checkStack.size()-1);
+    errors += verifyProcessState(current, 1, "asm1", checkRegisters, checkVariables, checkStack);
 
     if (errors.isEmpty()) errors = "  All tests passed!" + System.lineSeparator();
     return (header + errors);
