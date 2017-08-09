688187bee94d1e267b259ab8bb8e392e7f29fcad
JamesWWalker <jwwalker@mtu.edu>
2017-08-09 12:31:44 -0400

    Variable representation converter now appears to be fully functional

--- TestVariableRepresentation.java
+++ TestVariableRepresentation.java
@@ -9,88 +9,219 @@ public class TestVariableRepresentation {
     // First, simply check that inputted values are converted to correct hex values
 
     // shorts
-    VariableRepresentation rep = new VariableRepresentation(VariableType.SIGNED_SHORT, "-5");
+    VariableRepresentation rep = new VariableRepresentation(VariableType.SIGNED_SHORT, "-5", true);
     if (!rep.getValue().equals("0xFFFB"))
       errors += "  VALUE ERROR 1: Value is [" + rep.getValue() + "]" + System.lineSeparator();
 
-    rep.type = VariableType.UNSIGNED_SHORT;
-    rep.setValue("133");
+    rep.setValue("133", VariableType.UNSIGNED_SHORT);
     if (!rep.getValue().equals("0x0085"))
       errors += "  VALUE ERROR 2: Value is [" + rep.getValue() + "]" + System.lineSeparator();
       
-    rep.type = VariableType.UNSIGNED_SHORT;
-    rep.setValue("65000");
+    rep.setValue("65000", VariableType.UNSIGNED_SHORT);
     if (!rep.getValue().equals("0xFDE8"))
       errors += "  VALUE ERROR 3: Value is [" + rep.getValue() + "]" + System.lineSeparator();
       
     // ints
-    rep.type = VariableType.SIGNED_INT;
-    rep.setValue("-80000");
+    rep.setValue("-80000", VariableType.SIGNED_INT);
     if (!rep.getValue().equals("0xFFFEC780"))
       errors += "  VALUE ERROR 4: Value is [" + rep.getValue() + "]" + System.lineSeparator();
 
-    rep.type = VariableType.UNSIGNED_INT;
-    rep.setValue("32767");
+    rep.setValue("32767", VariableType.UNSIGNED_INT);
     if (!rep.getValue().equals("0x00007FFF"))
       errors += "  VALUE ERROR 5: Value is [" + rep.getValue() + "]" + System.lineSeparator();
       
-    rep.type = VariableType.UNSIGNED_INT;
-    rep.setValue("4294967200");
+    rep.setValue("4294967200", VariableType.UNSIGNED_INT);
     if (!rep.getValue().equals("0xFFFFFFA0"))
       errors += "  VALUE ERROR 6: Value is [" + rep.getValue() + "]" + System.lineSeparator();
       
     // longs
-    rep.type = VariableType.SIGNED_LONG;
-    rep.setValue("-789123456789");
+    rep.setValue("-789123456789", VariableType.SIGNED_LONG);
     if (!rep.getValue().equals("0xFFFFFF484493A0EB"))
       errors += "  VALUE ERROR 7: Value is [" + rep.getValue() + "]" + System.lineSeparator();
 
-    rep.type = VariableType.UNSIGNED_LONG;
-    rep.setValue("74175");
+    rep.setValue("74175", VariableType.UNSIGNED_LONG);
     if (!rep.getValue().equals("0x00000000000121BF"))
       errors += "  VALUE ERROR 8: Value is [" + rep.getValue() + "]" + System.lineSeparator();
       
-    rep.type = VariableType.UNSIGNED_LONG;
-    rep.setValue("9123372036854775807");
+    rep.setValue("9123372036854775807", VariableType.UNSIGNED_LONG);
     if (!rep.getValue().equals("0x7E9CBA87A275FFFF"))
       errors += "  VALUE ERROR 9: Value is [" + rep.getValue() + "]" + System.lineSeparator();
       
     // strings
-    rep.type = VariableType.STRING;
-    rep.setValue("Sonic, Tails, & Knuckles");
+    rep.setValue("Sonic, Tails, & Knuckles", VariableType.STRING);
     if (!rep.getValue().equals("Sonic, Tails, & Knuckles"))
       errors += "  VALUE ERROR 10: Value is [" + rep.getValue() + "]" + System.lineSeparator();
     
-    rep.type = VariableType.STRING;
-    rep.setValue("0xCAFE");
+    rep.setValue("0xcafe", VariableType.STRING);
     if (!rep.getValue().equals("0xCAFE"))
       errors += "  VALUE ERROR 11: Value is [" + rep.getValue() + "]" + System.lineSeparator();
       
     // chars
-    rep.type = VariableType.SIGNED_CHAR;
-    rep.setValue("'t'");
+    rep.setValue("'t'", VariableType.SIGNED_CHAR);
     if (!rep.getValue().equals("0x74"))
       errors += "  VALUE ERROR 12: Value is [" + rep.getValue() + "]" + System.lineSeparator();
       
-    rep.type = VariableType.SIGNED_CHAR;
-    rep.setValue("'\\x19'");
+    rep.setValue("'\\x19'", VariableType.SIGNED_CHAR);
     if (!rep.getValue().equals("0x19"))
       errors += "  VALUE ERROR 13: Value is [" + rep.getValue() + "]" + System.lineSeparator();
     
     
-    rep.type = VariableType.SIGNED_CHAR;
-    rep.setValue("'5'");
+    rep.setValue("'5'", VariableType.SIGNED_CHAR);
     if (!rep.getValue().equals("0x35"))
       errors += "  VALUE ERROR 14: Value is [" + rep.getValue() + "]" + System.lineSeparator();
       
-    // TODO
+    // Now check conversions
+    String conversion;
     
-    /*
-0x00007fffffffdd6d: (char) x = 't'
-0x00007fffffffdd6e: (char) y = '\x19'
-0x00007fffffffdd6f: (char) z = '5'
-
-*/
+    rep.setValue("2345678901", VariableType.UNSIGNED_INT);
+    if (!rep.getValue().equals("0x8BD03835"))
+      errors += "  VALUE ERROR 15: Value is [" + rep.getValue() + "]" + System.lineSeparator();
+      
+    conversion = rep.getDecimal(VariableType.UNSIGNED_INT, true);
+    if (!conversion.equals("2345678901"))
+      errors += "  VALUE ERROR 16: Value is [" + conversion + "]" + System.lineSeparator();
+    
+    conversion = rep.getHex(VariableType.UNSIGNED_INT, true);
+    if (!conversion.equals("0x8BD03835"))
+      errors += "  VALUE ERROR 17: Value is [" + conversion + "]" + System.lineSeparator();
+      
+    conversion = rep.getBinary(VariableType.UNSIGNED_INT, true);
+    if (!conversion.equals("10001011110100000011100000110101"))
+      errors += "  VALUE ERROR 18: Value is [" + conversion + "]" + System.lineSeparator();
+    
+    conversion = rep.getDecimal(VariableType.UNSIGNED_INT, false);
+    if (!conversion.equals("892915851"))
+      errors += "  VALUE ERROR 19: Value is [" + conversion + "]" + System.lineSeparator();
+    
+    conversion = rep.getHex(VariableType.UNSIGNED_INT, false);
+    if (!conversion.equals("0x3538D08B"))
+      errors += "  VALUE ERROR 20: Value is [" + conversion + "]" + System.lineSeparator();
+      
+    conversion = rep.getBinary(VariableType.UNSIGNED_INT, false);
+    if (!conversion.equals("00110101001110001101000010001011"))
+      errors += "  VALUE ERROR 21: Value is [" + conversion + "]" + System.lineSeparator();
+      
+    conversion = rep.getDecimal(VariableType.UNSIGNED_SHORT, true);
+    if (!conversion.equals("14389"))
+      errors += "  VALUE ERROR 22: Value is [" + conversion + "]" + System.lineSeparator();
+      
+    conversion = rep.getHex(VariableType.UNSIGNED_SHORT, true);
+    if (!conversion.equals("0x3835"))
+      errors += "  VALUE ERROR 23: Value is [" + conversion + "]" + System.lineSeparator();
+      
+    conversion = rep.getBinary(VariableType.UNSIGNED_SHORT, true);
+    if (!conversion.equals("0011100000110101"))
+      errors += "  VALUE ERROR 24: Value is [" + conversion + "]" + System.lineSeparator();
+    
+    conversion = rep.getDecimal(VariableType.UNSIGNED_CHAR, true);
+    if (!conversion.equals("53"))
+      errors += "  VALUE ERROR 25: Value is [" + conversion + "]" + System.lineSeparator();
+      
+    conversion = rep.getHex(VariableType.UNSIGNED_CHAR, true);
+    if (!conversion.equals("0x35"))
+      errors += "  VALUE ERROR 26: Value is [" + conversion + "]" + System.lineSeparator();
+      
+    conversion = rep.getBinary(VariableType.UNSIGNED_CHAR, true);
+    if (!conversion.equals("00110101"))
+      errors += "  VALUE ERROR 27: Value is [" + conversion + "]" + System.lineSeparator();
+      
+    conversion = rep.getDecimal(VariableType.SIGNED_SHORT, true);
+    if (!conversion.equals("14389"))
+      errors += "  VALUE ERROR 28: Value is [" + conversion + "]" + System.lineSeparator();
+      
+    conversion = rep.getHex(VariableType.SIGNED_SHORT, true);
+    if (!conversion.equals("0x3835"))
+      errors += "  VALUE ERROR 29: Value is [" + conversion + "]" + System.lineSeparator();
+      
+    // Now let's try a big negative value.
+    rep.isBigEndian = false;
+    rep.setValue("-999999999", VariableType.SIGNED_LONG);
+    if (!rep.getValue().equals("0xFFFFFFFFC4653601"))
+      errors += "  VALUE ERROR 30: Value is [" + rep.getValue() + "]" + System.lineSeparator();
+      
+    conversion = rep.getDecimal(VariableType.SIGNED_LONG, false);
+    if (!conversion.equals("-999999999"))
+      errors += "  VALUE ERROR 31: Value is [" + conversion + "]" + System.lineSeparator();
+      
+    conversion = rep.getDecimal(VariableType.UNSIGNED_LONG, false);
+    if (!conversion.equals("18446744072709551617"))
+      errors += "  VALUE ERROR 32: Value is [" + conversion + "]" + System.lineSeparator();
+    
+    conversion = rep.getHex(VariableType.SIGNED_LONG, true);
+    if (!conversion.equals("0x013665C4FFFFFFFF"))
+      errors += "  VALUE ERROR 33: Value is [" + conversion + "]" + System.lineSeparator();
+    
+    conversion = rep.getHex(VariableType.UNSIGNED_LONG, true);
+    if (!conversion.equals("0x013665C4FFFFFFFF"))
+      errors += "  VALUE ERROR 34: Value is [" + conversion + "]" + System.lineSeparator();
+    
+    conversion = rep.getDecimal(VariableType.SIGNED_LONG, true);
+    if (!conversion.equals("87369139563266047"))
+      errors += "  VALUE ERROR 35: Value is [" + conversion + "]" + System.lineSeparator();
+    
+    conversion = rep.getDecimal(VariableType.UNSIGNED_LONG, true);
+    if (!conversion.equals("87369139563266047"))
+      errors += "  VALUE ERROR 36: Value is [" + conversion + "]" + System.lineSeparator();
+      
+    conversion = rep.getHex(VariableType.SIGNED_INT, true);
+    if (!conversion.equals("0xFFFFFFFF"))
+      errors += "  VALUE ERROR 37: Value is [" + conversion + "]" + System.lineSeparator();
+    
+    conversion = rep.getDecimal(VariableType.UNSIGNED_INT, true);
+    if (!conversion.equals("4294967295"))
+      errors += "  VALUE ERROR 38: Value is [" + conversion + "]" + System.lineSeparator();
+      
+    conversion = rep.getBinary(VariableType.SIGNED_INT, true);
+    if (!conversion.equals("11111111111111111111111111111111"))
+      errors += "  VALUE ERROR 39: Value is [" + conversion + "]" + System.lineSeparator();
+    
+    conversion = rep.getDecimal(VariableType.SIGNED_INT, true);
+    if (!conversion.equals("-1"))
+      errors += "  VALUE ERROR 40: Value is [" + conversion + "]" + System.lineSeparator();
+      
+    conversion = rep.getHex(VariableType.SIGNED_SHORT, false);
+    if (!conversion.equals("0x3601"))
+      errors += "  VALUE ERROR 41: Value is [" + conversion + "]" + System.lineSeparator();
+    
+    conversion = rep.getDecimal(VariableType.SIGNED_SHORT, false);
+    if (!conversion.equals("13825"))
+      errors += "  VALUE ERROR 42: Value is [" + conversion + "]" + System.lineSeparator();
+    
+    // Check to make sure small values get padded out when converted to larger ones
+    rep.isBigEndian = true;
+    rep.setValue("'#'", VariableType.SIGNED_CHAR); // ASCII code 35
+    if (!rep.getValue().equals("0x23"))
+      errors += "  VALUE ERROR 43: Value is [" + rep.getValue() + "]" + System.lineSeparator();
+    
+    conversion = rep.getHex(VariableType.SIGNED_SHORT, true);
+    if (!conversion.equals("0x0023"))
+      errors += "  VALUE ERROR 44: Value is [" + conversion + "]" + System.lineSeparator();
+    
+    conversion = rep.getBinary(VariableType.SIGNED_SHORT, true);
+    if (!conversion.equals("0000000000100011"))
+      errors += "  VALUE ERROR 45: Value is [" + conversion + "]" + System.lineSeparator();
+    
+    conversion = rep.getHex(VariableType.SIGNED_INT, true);
+    if (!conversion.equals("0x00000023"))
+      errors += "  VALUE ERROR 46: Value is [" + conversion + "]" + System.lineSeparator();
+      
+    conversion = rep.getBinary(VariableType.SIGNED_INT, true);
+    if (!conversion.equals("00000000000000000000000000100011"))
+      errors += "  VALUE ERROR 47: Value is [" + conversion + "]" + System.lineSeparator();
+    
+    conversion = rep.getHex(VariableType.SIGNED_LONG, true);
+    if (!conversion.equals("0x0000000000000023"))
+      errors += "  VALUE ERROR 48: Value is [" + conversion + "]" + System.lineSeparator();
+    
+    conversion = rep.getBinary(VariableType.SIGNED_LONG, true);
+    if (!conversion.equals("0000000000000000000000000000000000000000000000000000000000100011"))
+      errors += "  VALUE ERROR 49: Value is [" + conversion + "]" + System.lineSeparator();
+    
+    conversion = rep.getDecimal(VariableType.UNSIGNED_LONG, true);
+    if (!conversion.equals("35"))
+      errors += "  VALUE ERROR 50: Value is [" + conversion + "]" + System.lineSeparator();
+    
+    // TODO: add address
 
     if (errors.isEmpty()) errors = "  All tests passed!" + System.lineSeparator();
     return (header + errors);
--- VariableRepresentation.java
+++ VariableRepresentation.java
@@ -1,45 +1,53 @@
+import java.math.*;
+import java.util.*;
+
 public class VariableRepresentation {
 
   String value;
-  public VariableType type;
+  VariableType type;
+  public boolean isBigEndian;
+  public long address;
   
   
   public String getValue() { return value; }
   
   
-  public void setValue(String valueIn) {
-    if (!valueIn.startsWith("0x") && type != VariableType.STRING) {
+  public void setValue(String valueIn, VariableType typeIn) {
+    type = typeIn;
+    if (valueIn.startsWith("0x") && type == VariableType.STRING)
+      value = "0x" + valueIn.substring(2).toUpperCase();
+    else if (!valueIn.startsWith("0x") && type != VariableType.STRING) {
       switch (type) {
         case SIGNED_CHAR:
         case UNSIGNED_CHAR:
           value = valueIn.replaceAll("'", "");
           if (value.startsWith("\\x")) value = value.substring(2);
           else value = Integer.toHexString((int)(value.charAt(0)));
-          value = "00".substring(value.length()) + value; // pad out with 0s
+          value = padWithZeroes(2, value);
           break;
         case SIGNED_SHORT:
           value = Integer.toHexString(Short.parseShort(valueIn) & 0xffff);
-          value = "0000".substring(value.length()) + value; // pad out with 0s
+          value = padWithZeroes(4, value);
           break;
         case UNSIGNED_SHORT:
           value = Integer.toHexString(Integer.parseUnsignedInt(valueIn) & 0xffff);
-          value = "0000".substring(value.length()) + value; // pad out with 0s
+          value = padWithZeroes(4, value);
           break;
         case SIGNED_INT:
           value = Integer.toHexString(Integer.parseInt(valueIn));
-          value = "00000000".substring(value.length()) + value; // pad out with 0s
+          value = padWithZeroes(8, value);
           break;
         case UNSIGNED_INT:
           value = Integer.toHexString(Integer.parseUnsignedInt(valueIn));
-          value = "00000000".substring(value.length()) + value; // pad out with 0s
+          value = padWithZeroes(8, value);
           break;
         case SIGNED_LONG:
           value = Long.toHexString(Long.parseLong(valueIn));
-          value = "0000000000000000".substring(value.length()) + value; // pad out with 0s
+          value = padWithZeroes(16, value);
           break;
         case UNSIGNED_LONG:
           value = Long.toHexString(Long.parseUnsignedLong(valueIn));
-          value = "0000000000000000".substring(value.length()) + value; // pad out with 0s
+          value = padWithZeroes(16, value);
           break;
         default:
           value = valueIn;
@@ -51,19 +59,114 @@ public class VariableRepresentation {
   }
   
   
-  public VariableRepresentation(VariableType typeIn, String valueIn) {
-    type = typeIn;
-    setValue(valueIn);
+  public VariableRepresentation(VariableType typeIn, String valueIn, boolean isBigEndianIn) {
+    setValue(valueIn, typeIn);
+    isBigEndian = isBigEndianIn;
   }
   
   
-  String asSignedChar() {
-    switch (type) {
+  public String getDecimal(VariableType convertTo, boolean convertToBigEndian) {
+    if (type == VariableType.STRING || convertTo == VariableType.STRING) return value;
+    String hex = getHex(convertTo, convertToBigEndian).substring(2);
+    
+    switch (convertTo) {
       case SIGNED_CHAR:
-        return "TODO";
+        return "" + (byte)(new BigInteger(hex, 16).intValue());
+      case SIGNED_SHORT:
+        return Short.toString((short)(new BigInteger(hex, 16).intValue()));
+      case SIGNED_INT:
+        return Integer.toString(new BigInteger(hex, 16).intValue());
+      case SIGNED_LONG:
+        return Long.toString(new BigInteger(hex, 16).longValue());
+      case UNSIGNED_CHAR:
+        return "" + (byte)(new BigInteger(hex, 16).intValue());
+      case UNSIGNED_SHORT:
+        return Integer.toUnsignedString((short)(new BigInteger(hex, 16).intValue()));
+      case UNSIGNED_INT:
+        return Integer.toUnsignedString(new BigInteger(hex, 16).intValue());
+      case UNSIGNED_LONG:
+        return Long.toUnsignedString(new BigInteger(hex, 16).longValue());
       default:
-        return "TODO"; // TODO
+        return value;
     }
+  }
+  
+  
+  public String getHex(VariableType convertTo, boolean convertToBigEndian) {
+    if (type == VariableType.STRING || convertTo == VariableType.STRING) return value;
+    List<String> bytes = divideHexIntoBytes(value.substring(2));
+    if (convertToBigEndian != isBigEndian) Collections.reverse(bytes);
+    String joinedBytes = getBytesByType(String.join("", bytes), convertTo);
+    
+    return "0x" + joinedBytes;
+  }
+  
+  
+  public String getBinary(VariableType convertTo, boolean convertToBigEndian) {
+    if (type == VariableType.STRING || convertTo == VariableType.STRING) return value;
+    String hex = getHex(convertTo, convertToBigEndian).substring(2);
+    
+    int numZeroes = 0;
+    switch (convertTo) {
+      case UNSIGNED_CHAR:
+      case SIGNED_CHAR: numZeroes = 8; break;
+      case SIGNED_SHORT: 
+      case UNSIGNED_SHORT: numZeroes = 16; break;
+      case SIGNED_INT: 
+      case UNSIGNED_INT: numZeroes = 32; break;
+      case SIGNED_LONG: 
+      case UNSIGNED_LONG: numZeroes = 64; break;
+    }
+    return padWithZeroes(numZeroes, new BigInteger(hex, 16).toString(2));
+  }
+  
+  
+  String getBytesByType(String digits, VariableType convertTo) {
+    int numDigits;
+    switch (convertTo) {
+      case SIGNED_CHAR:
+      case UNSIGNED_CHAR:
+        numDigits = 2;
+        if (digits.length() >= numDigits) digits = digits.substring(digits.length()-numDigits);
+        else digits = padWithZeroes(numDigits, digits);
+        break;
+      case SIGNED_SHORT:
+      case UNSIGNED_SHORT:
+        numDigits = 4;
+        if (digits.length() >= numDigits) digits = digits.substring(digits.length()-numDigits);
+        else digits = padWithZeroes(numDigits, digits);
+        break;
+      case SIGNED_INT:
+      case UNSIGNED_INT:
+        numDigits = 8;
+        if (digits.length() >= numDigits) digits = digits.substring(digits.length()-numDigits);
+        else digits = padWithZeroes(numDigits, digits);
+        break;
+      case SIGNED_LONG:
+      case UNSIGNED_LONG:
+        numDigits = 16;
+        if (digits.length() >= numDigits) digits = digits.substring(digits.length()-numDigits);
+        else digits = padWithZeroes(numDigits, digits);
+        break;        
+    }
+    return digits;
+  }
+  
+  
+  List<String> divideHexIntoBytes(String hex) {
+    List<String> bytes = new ArrayList<>();
+    while (hex.length() > 0) {
+      bytes.add(hex.substring(0, 2));
+      hex = hex.substring(2, hex.length());
+    }
+    return bytes;
+  }
+  
+  
+  String padWithZeroes(int numZeroes, String str) {
+    String zeroes = "";
+    for (int n = 0; n < numZeroes; ++n) zeroes += "0";
+    return zeroes.substring(str.length()) + str;
   }
 
 }
@@ -76,7 +179,6 @@ public enum VariableType {
   SIGNED_SHORT, UNSIGNED_SHORT,
   SIGNED_INT, UNSIGNED_INT,
   SIGNED_LONG, UNSIGNED_LONG,
-  SIGNED_LONG_LONG, UNSIGNED_LONG_LONG,
   STRING; 
 }
 */
