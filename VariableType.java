7f76759ec67d9ff04546b15e6d3e48b4776c71e2
JamesWWalker <jwwalker@mtu.edu>
2017-09-04 14:57:45 -0400

    Fixed a variable representation type bug

--- VariableType.java
+++ VariableType.java
@@ -16,6 +16,8 @@ public enum VariableType {
         }
         return SIGNED_LONG;
       }
+      if (in.contains("long")) return SIGNED_LONG;
+      if (in.contains("short")) return SIGNED_SHORT;
     } 
     else {
       if (in.contains("char")) return UNSIGNED_CHAR;
@@ -26,7 +28,25 @@ public enum VariableType {
         }
         return UNSIGNED_LONG;
       }
+      if (in.contains("long")) return UNSIGNED_LONG;
+      if (in.contains("short")) return UNSIGNED_SHORT;
     }
     return STRING;
+  }
+  
+  
+  public static String toString(VariableType type) {
+    switch (type) {
+      case SIGNED_CHAR: return "signed char";
+      case UNSIGNED_CHAR: return "unsigned char";
+      case SIGNED_SHORT: return "signed short";
+      case UNSIGNED_SHORT: return "unsigned short";
+      case SIGNED_INT: return "signed int"; 
+      case UNSIGNED_INT: return "unsigned int";
+      case SIGNED_LONG: return "signed long";
+      case UNSIGNED_LONG: return "unsigned long";
+      case STRING: return "string";
+      default: return "invalid";
+    }
   }
 }
